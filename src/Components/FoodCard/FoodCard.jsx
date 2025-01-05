4ad93fab5666efb693553ff3e0546286e37b2c74
NureAlam68 <nurealam151068@gmail.com>
2025-01-05 16:51:36 +0600

    added cart in navbar

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,7 +1,33 @@
 import PropTypes from "prop-types";
+import useAuth from "../../hooks/useAuth";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const FoodCard = ({item}) => {
-    const { name, image, price, recipe } = item
+    const { name, image, price, recipe } = item;
+    const { user } = useAuth();
+    const navigate = useNavigate();
+
+    const handleAddToCart = food => {
+      if(user && user.email) {
+        ""
+      }
+      else{
+        Swal.fire({
+          title: "You are not Logged In!",
+          text: "Please login for add to the cart.",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, login!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            navigate("/login")
+          }
+        });
+      }
+    }
   return (
     <div className="card bg-[#F3F3F3] rounded-none">
       <figure>
@@ -17,7 +43,9 @@ const FoodCard = ({item}) => {
         <h2 className="font-inter text-[24px] font-semibold text-center">{name}</h2>
         <p className="font-inter text-[#737373]">{recipe}</p>
         <div className="card-actions justify-center mt-6">
-          <button className="bg-[#E8E8E8] px-[30px] py-[20px] rounded-[8px] text-[20px] text-[#BB8506] font-inter font-medium border-b-[3px] border-[#BB8506] hover:bg-[#111827]">ADD TO CART</button>
+          <button
+          onClick={() => handleAddToCart(item)}
+          className="bg-[#E8E8E8] px-[30px] py-[20px] rounded-[8px] text-[20px] text-[#BB8506] font-inter font-medium border-b-[3px] border-[#BB8506] hover:bg-[#111827]">ADD TO CART</button>
         </div>
       </div>
     </div>
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css"
 import { AuthContext } from "../../../providers/AuthProvider";
 import { useContext } from "react";
@@ -24,6 +24,27 @@ const Navbar = () => {
       <li>
         <NavLink to="/order/salad">Order Food</NavLink>
       </li>
+      <li>
+        <Link>
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+        <div className="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+          <span className="badge badge-sm indicator-item">0</span>
+        </div>
+      </div>
+        </Link>
+      </li>
       {
         user ? (
           <li>
@@ -39,7 +60,7 @@ const Navbar = () => {
   );
   return (
     <>
-      <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white py-1 md:py-3 lg:py-5">
+      <div className="navbar fixed z-10 bg-opacity-30 bg-black text-white py-1 md:py-3 lg:py-5 items-center">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -75,7 +96,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="flex gap-4 uppercase px-1 font-inter font-extrabold">
+          <ul className="flex gap-4 uppercase px-1 font-inter font-extrabold items-center">
             {navOptions}
           </ul>
         </div>
