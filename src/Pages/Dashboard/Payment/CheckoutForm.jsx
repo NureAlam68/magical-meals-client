2993ed1e89e5bf90b72a44082aae13f42a3d6c7b
NureAlam68 <nurealam151068@gmail.com>
2025-01-15 23:38:52 +0600

    added stripe payment

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,8 +1,10 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
+  const [error, setError] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -19,29 +21,64 @@ const CheckoutForm = () => {
     if (card === null) {
       return;
     }
+    // Use your card Element with other Stripe.js APIs
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card,
+    });
+    if (error) {
+      setError(error.message)
+    } else {
+      console.log('[PaymentMethod]', paymentMethod);
+      setError('')
+    }
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <div className="mt-10 md:mt-20 px-5 md:px-10">
+        <form onSubmit={handleSubmit} className="space-y-4">
+      <div className="flex space-x-4">
+        <div className="flex items-center border rounded-lg px-3 py-2 w-1/2 bg-white">
+          <span className="text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5 mr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 9h19.5M2.25 12h19.5M2.25 15h19.5"
+              />
+            </svg>
+          </span>
+          <CardElement
+            options={{
+              style: {
+                base: {
+                  fontSize: "16px",
+                  color: "#424770",
+                  "::placeholder": { color: "#aab7c4" },
+                },
+                invalid: { color: "#9e2146" },
               },
-            },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <button type="submit" disabled={!stripe}>
+            }}
+            className="w-full"
+          />
+        </div>
+      </div>
+      <button
+        type="submit"
+        disabled={!stripe}
+        className="bg-[#570DF8] text-white py-2 px-20 rounded-lg font-semibold hover:bg-purple-700 disabled:bg-gray-400"
+      >
         Pay
       </button>
+      <p className="text-red-600">{error}</p>
     </form>
+    </div>
   );
 };
 
--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,16 +1,17 @@
 import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
 
 
-const stripePromise = loadStripe();
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
     return (
         <div>
             <SectionTitle heading={'PAYMENT'} subHeading={'Please pay to eat!'}></SectionTitle>
             <div>
                 <Elements stripe={stripePromise}>
-
+                    <CheckoutForm></CheckoutForm>
                 </Elements>
             </div>
         </div>
